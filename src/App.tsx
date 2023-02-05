74791afbd538e4fcbb82ace556ba54349977890b
austin-jrh <austin.jrh@gmail.com>
2023-02-05 12:37:01 +0800

    Fixed eslint bug and keyboard bug
    Fixed eslint bug and keyboard bug where game continues to take keyboard input after lose/win scenario is hit.

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
     return () => {
       document.removeEventListener("keypress", handler);
     };
-  }, [addGuessedLetter]);
+  }, [guessedLetters]);
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
