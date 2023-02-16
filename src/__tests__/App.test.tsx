a216b9f7b61eadcef215e4b605b8f563c0f1cdef
austin-jrh <austin.jrh@gmail.com>
2023-02-17 00:56:34 +0800

    Update App.test.tsx

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -32,13 +32,29 @@ test("when click 'a' key, it should appear either in word or in drawing", () => 
   const key_a = screen.getByTestId("key-a");
   fireEvent.click(key_a);
 
+  const wordToGuess = screen.getAllByTestId(/letter-index-*/);
   if (
-    screen.getAllByTestId(/letter-index-*/).some((letter) => {
+    wordToGuess.some((letter) => {
       return letter.textContent === "a";
     })
   ) {
     console.log("appear in word");
-    expect(true).toBe(true); // appear in HangmanWord
+
+    // letter 'a' is visible
+    const indexForA = wordToGuess
+      .map((letter, i) => (letter.textContent === "a" ? i : ""))
+      .filter(String);
+    indexForA.forEach((i) => {
+      expect(screen.getByTestId(`letter-index-${i}`)).toBeVisible();
+    });
+
+    // remaining letters are not visible
+    const otherIndices = wordToGuess
+      .map((letter, i) => (letter.textContent !== "a" ? i : ""))
+      .filter(String);
+    otherIndices.forEach((i) => {
+      expect(screen.getByTestId(`letter-index-${i}`)).not.toBeVisible();
+    });
   } else {
     console.log("appear in drawing");
     expect(screen.getByTestId("hman-head")).toBeInTheDocument(); // appear in HangmanDrawing
